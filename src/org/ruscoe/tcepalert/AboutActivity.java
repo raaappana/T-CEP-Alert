a5b97d7d7ef456b2568085973073a5a6f699d5c9
Dan Ruscoe <dan@ruscoe.org>
2012-06-16 17:44:43 -0700

    Enabled links in AboutActivity.

--- src/org/ruscoe/tcepalert/AboutActivity.java
+++ src/org/ruscoe/tcepalert/AboutActivity.java
@@ -2,6 +2,8 @@ package org.ruscoe.tcepalert;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 /**
  * An activity used to display information about the application.
@@ -16,5 +18,8 @@ public class AboutActivity extends Activity
     	super.onCreate(savedInstanceState);
     	
     	setContentView(R.layout.about);
+    	
+    	TextView aboutUrl = (TextView) findViewById(R.id.aboutUrl);
+    	aboutUrl.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
